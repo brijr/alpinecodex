f25c0d5cf1169860f1d581ba10e950e61e35a824
Bridger Tower <bridgertower@gmail.com>
2023-07-05 15:50:28 -0600

    First

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import './styles.css';
+	import Alpine from '$lib/images/Alpine.svg';
+	import Codex from '$lib/images/Codex.svg';
+</script>
+
+<main
+	class="bg-[#0500FF] text-white min-w-screen min-h-screen w-full h-full flex justify-center items-center flex-col"
+>
+	<img src={Alpine} class="fixed top-0 left-0 md:w-64" alt="Alpine" />
+	<slot />
+	<img src={Codex} class="fixed bottom-0 right-0 md:w-64" alt="Codex" />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<section>
+	<h1 class="text-2xl font-mono">Alpine Codex</h1>
+	<p class="font-mono mt-12">Applied Artificial Intelligence</p>
+</section>
